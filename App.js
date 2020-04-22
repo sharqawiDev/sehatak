ee8df1fe83cb0789dce49ccd00da0f09563b758a
Mohammed Salmeen <m.salmeen@uxbert.com>
2020-04-22 22:07:37 +0300

    las update

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable radix */
 /* eslint-disable no-lone-blocks */
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
@@ -15,7 +16,7 @@ import {
 } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-// import groupImage from './images/Group5Copy3.png';
+import {LineChart} from 'react-native-chart-kit';
 
 const {width, height} = Dimensions.get('window');
 function HomeScreen({navigation}) {
@@ -74,10 +75,10 @@ function HomeScreen({navigation}) {
             alignSelf: 'flex-start',
             marginLeft: 10,
           }}>
-          Email Address
+          ID Number
         </Text>
         <TextInput
-          placeholder="Example@gmail.com"
+          placeholder="123456789"
           style={{
             backgroundColor: '#fff',
             width: '100%',
@@ -86,7 +87,7 @@ function HomeScreen({navigation}) {
             paddingLeft: 10,
             marginTop: 10,
           }}
-          keyboardType="email-address"
+          keyboardType="number-pad"
           onChangeText={(val) => setEmail(val)}
           value={email}
           maxLength={40}
@@ -112,7 +113,6 @@ function DetailsScreen({navigation, route}) {
         <View
           style={{
             alignItems: 'center',
-            marginBottom: 30,
             paddingBottom: 20,
           }}>
           <ImageBackground
@@ -129,13 +129,15 @@ function DetailsScreen({navigation, route}) {
             }}>
             <Text
               style={{
-                fontSize: 30,
-                alignSelf: 'center',
+                fontSize: 45,
                 color: '#fff',
-                marginTop: 40,
+                marginTop: 140,
+                marginLeft: 30,
+                fontWeight: '300',
+                // fontFamily: 'arial', //tahoma, verdana, arial;
               }}>
               Good Morning {'\n'}
-              {JSON.stringify(name)} {'\n'}
+              {name}
             </Text>
             <View
               style={{
@@ -143,22 +145,34 @@ function DetailsScreen({navigation, route}) {
                 justifyContent: 'space-between',
                 alignSelf: 'center',
                 width: '90%',
-                marginTop: 150,
+                marginTop: 50,
               }}>
               <TouchableOpacity style={styles.card}>
                 <View
                   style={{
                     flexDirection: 'row',
                     justifyContent: 'space-between',
+                    marginTop: 10,
                   }}>
-                  <Text style={{alignSelf: 'center', fontSize: 18}}>Pulse</Text>
+                  <Text
+                    style={{
+                      alignSelf: 'center',
+                      fontSize: 16,
+                      marginLeft: 10,
+                      fontWeight: '600',
+                    }}>
+                    Oxygen
+                  </Text>
                   <Image
                     source={require('./images/oxygen.png')}
                     style={styles.imageStyle}
                   />
                 </View>
-                <View style={{flexDirection: 'row'}}>
-                  <Text style={{fontWeight: '500', fontSize: 25}}> 122</Text>
+                <View
+                  style={{flexDirection: 'row', marginTop: 3, marginLeft: 10}}>
+                  <Text style={{fontWeight: '500', fontSize: 25}}>
+                    {parseInt(75 + Math.random() * (100 - 75))}
+                  </Text>
                   <Text
                     style={{
                       fontWeight: '500',
@@ -167,24 +181,86 @@ function DetailsScreen({navigation, route}) {
                       alignSelf: 'center',
                       marginLeft: 5,
                     }}>
-                    BPM
+                    mL / mmHg
                   </Text>
                 </View>
+                <LineChart
+                  data={{
+                    labels: [
+                      'January',
+                      'February',
+                      'March',
+                      'April',
+                      'May',
+                      'June',
+                    ],
+                    datasets: [
+                      {
+                        data: [
+                          Math.random() * 100,
+                          Math.random() * 100,
+                          Math.random() * 100,
+                          Math.random() * 100,
+                          Math.random() * 100,
+                          Math.random() * 100,
+                        ],
+                      },
+                    ],
+                  }}
+                  width={172} // from react-native
+                  height={100}
+                  yAxisInterval={1} // optional, defaults to 1
+                  chartConfig={{
+                    backgroundColor: '#000',
+                    backgroundGradientFrom: '#fff',
+                    backgroundGradientTo: '#fff',
+                    decimalPlaces: 2, // optional, defaults to 2dp
+                    color: (opacity = 1) => `rgba(000, 000, 000, ${opacity})`,
+                    labelColor: (opacity = 1) =>
+                      `rgba(000, 000, 000, ${opacity})`,
+                    style: {
+                      borderRadius: 16,
+                    },
+                    propsForDots: {
+                      r: '3',
+                      strokeWidth: '1',
+                      stroke: '#000',
+                    },
+                  }}
+                  bezier
+                  style={{
+                    marginVertical: 8,
+                    borderRadius: 16,
+                    alignSelf: 'center',
+                  }}
+                />
               </TouchableOpacity>
               <TouchableOpacity style={styles.card}>
                 <View
                   style={{
                     flexDirection: 'row',
                     justifyContent: 'space-between',
+                    marginTop: 10,
                   }}>
-                  <Text style={{alignSelf: 'center', fontSize: 18}}>Pulse</Text>
+                  <Text
+                    style={{
+                      alignSelf: 'center',
+                      fontSize: 16,
+                      marginLeft: 10,
+                      fontWeight: '600',
+                    }}>
+                    Pulse
+                  </Text>
                   <Image
                     source={require('./images/pulse.png')}
                     style={styles.imageStyle}
                   />
                 </View>
-                <View style={{flexDirection: 'row'}}>
-                  <Text style={{fontWeight: '500', fontSize: 25}}> 122</Text>
+                <View
+                  style={{flexDirection: 'row', marginTop: 3, marginLeft: 10}}>
+                  <Text style={{fontWeight: '500', fontSize: 25}}>
+                    {parseInt(60 + Math.random() * (100 - 60))}
+                  </Text>
                   <Text
                     style={{
                       fontWeight: '500',
@@ -196,6 +272,56 @@ function DetailsScreen({navigation, route}) {
                     BPM
                   </Text>
                 </View>
+                <LineChart
+                  data={{
+                    labels: [
+                      'January',
+                      'February',
+                      'March',
+                      'April',
+                      'May',
+                      'June',
+                    ],
+                    datasets: [
+                      {
+                        data: [
+                          Math.random() * 100,
+                          Math.random() * 100,
+                          Math.random() * 100,
+                          Math.random() * 100,
+                          Math.random() * 100,
+                          Math.random() * 100,
+                        ],
+                      },
+                    ],
+                  }}
+                  width={172} // from react-native
+                  height={100}
+                  yAxisInterval={1} // optional, defaults to 1
+                  chartConfig={{
+                    backgroundColor: '#000',
+                    backgroundGradientFrom: '#fff',
+                    backgroundGradientTo: '#fff',
+                    decimalPlaces: 2, // optional, defaults to 2dp
+                    color: (opacity = 1) => `rgba(000, 000, 000, ${opacity})`,
+                    labelColor: (opacity = 1) =>
+                      `rgba(000, 000, 000, ${opacity})`,
+                    style: {
+                      borderRadius: 16,
+                    },
+                    propsForDots: {
+                      r: '3',
+                      strokeWidth: '1',
+                      stroke: '#000',
+                    },
+                  }}
+                  bezier
+                  style={{
+                    marginVertical: 8,
+                    borderRadius: 16,
+                    alignSelf: 'center',
+                  }}
+                />
               </TouchableOpacity>
             </View>
           </ImageBackground>
@@ -204,19 +330,34 @@ function DetailsScreen({navigation, route}) {
               flexDirection: 'row',
               justifyContent: 'space-between',
               width: '90%',
-              marginTop: 100,
+              marginTop: 80,
             }}>
             <TouchableOpacity style={styles.card}>
               <View
-                style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Text style={{alignSelf: 'center', fontSize: 18}}>Pulse</Text>
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  marginTop: 10,
+                }}>
+                <Text
+                  style={{
+                    alignSelf: 'center',
+                    fontSize: 16,
+                    marginLeft: 10,
+                    fontWeight: '600',
+                  }}>
+                  Temperature
+                </Text>
                 <Image
                   source={require('./images/temp.png')}
                   style={styles.imageStyle}
                 />
               </View>
-              <View style={{flexDirection: 'row'}}>
-                <Text style={{fontWeight: '500', fontSize: 25}}> 122</Text>
+              <View
+                style={{flexDirection: 'row', marginTop: 3, marginLeft: 10}}>
+                <Text style={{fontWeight: '500', fontSize: 25}}>
+                  {parseInt(36.1 + Math.random() * (37.2 - 36.1))}
+                </Text>
                 <Text
                   style={{
                     fontWeight: '500',
@@ -225,21 +366,87 @@ function DetailsScreen({navigation, route}) {
                     alignSelf: 'center',
                     marginLeft: 5,
                   }}>
-                  BPM
+                  °C
                 </Text>
               </View>
+              <LineChart
+                data={{
+                  labels: [
+                    'January',
+                    'February',
+                    'March',
+                    'April',
+                    'May',
+                    'June',
+                  ],
+                  datasets: [
+                    {
+                      data: [
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                      ],
+                    },
+                  ],
+                }}
+                width={172} // from react-native
+                height={100}
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                  backgroundColor: '#000',
+                  backgroundGradientFrom: '#fff',
+                  backgroundGradientTo: '#fff',
+                  decimalPlaces: 2, // optional, defaults to 2dp
+                  color: (opacity = 1) => `rgba(000, 000, 000, ${opacity})`,
+                  labelColor: (opacity = 1) =>
+                    `rgba(000, 000, 000, ${opacity})`,
+                  style: {
+                    borderRadius: 16,
+                  },
+                  propsForDots: {
+                    r: '3',
+                    strokeWidth: '1',
+                    stroke: '#000',
+                  },
+                }}
+                bezier
+                style={{
+                  marginVertical: 8,
+                  borderRadius: 16,
+                  alignSelf: 'center',
+                }}
+              />
             </TouchableOpacity>
             <TouchableOpacity style={styles.card}>
               <View
-                style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Text style={{alignSelf: 'center', fontSize: 18}}>Pulse</Text>
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  marginTop: 10,
+                }}>
+                <Text
+                  style={{
+                    alignSelf: 'center',
+                    fontSize: 16,
+                    marginLeft: 10,
+                    fontWeight: '600',
+                  }}>
+                  Blood{'\n'}Pressure
+                </Text>
                 <Image
                   source={require('./images/pressure.png')}
                   style={styles.imageStyle}
                 />
               </View>
-              <View style={{flexDirection: 'row'}}>
-                <Text style={{fontWeight: '500', fontSize: 25}}> 122</Text>
+              <View
+                style={{flexDirection: 'row', marginTop: 3, marginLeft: 10}}>
+                <Text style={{fontWeight: '500', fontSize: 25}}>
+                  {parseInt(120 + Math.random() * (139 - 120))} {'/'}
+                  {parseInt(80 + Math.random() * (89 - 80))}
+                </Text>
                 <Text
                   style={{
                     fontWeight: '500',
@@ -248,25 +455,137 @@ function DetailsScreen({navigation, route}) {
                     alignSelf: 'center',
                     marginLeft: 5,
                   }}>
-                  BPM
+                  mmHg
                 </Text>
               </View>
+              <LineChart
+                data={{
+                  labels: [
+                    'January',
+                    'February',
+                    'March',
+                    'April',
+                    'May',
+                    'June',
+                  ],
+                  datasets: [
+                    {
+                      data: [
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                      ],
+                    },
+                  ],
+                }}
+                width={172} // from react-native
+                height={100}
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                  backgroundColor: '#000',
+                  backgroundGradientFrom: '#fff',
+                  backgroundGradientTo: '#fff',
+                  decimalPlaces: 2, // optional, defaults to 2dp
+                  color: (opacity = 1) => `rgba(000, 000, 000, ${opacity})`,
+                  labelColor: (opacity = 1) =>
+                    `rgba(000, 000, 000, ${opacity})`,
+                  style: {
+                    borderRadius: 16,
+                  },
+                  propsForDots: {
+                    r: '3',
+                    strokeWidth: '1',
+                    stroke: '#000',
+                  },
+                }}
+                bezier
+                style={{
+                  marginVertical: 8,
+                  borderRadius: 16,
+                  alignSelf: 'center',
+                }}
+              />
             </TouchableOpacity>
           </View>
         </View>
       </View>
+      <Button
+        title="Do The Test"
+        color="#39b54a"
+        onPress={() => {
+          navigation.navigate('Test');
+        }}
+      />
     </ScrollView>
   );
 }
-
+function Testing({navigation}) {
+  return (
+    <View>
+      <Text
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignSelf: 'center',
+          fontSize: 30,
+          fontWeight: '500',
+        }}>
+        hi
+      </Text>
+    </View>
+  );
+}
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#006837',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#006837',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Test"
+          component={Testing}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#006837',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -274,16 +593,17 @@ function App() {
 
 const styles = StyleSheet.create({
   imageStyle: {
-    width: 30,
-    height: 30,
+    width: 40,
+    height: 40,
     borderRadius: 9,
     marginTop: 10,
     resizeMode: 'contain',
     backgroundColor: '#fff',
+    marginRight: 10,
   },
   card: {
     backgroundColor: '#FFF',
-    width: width * 0.35,
+    width: width * 0.42,
     height: 200,
     borderRadius: 20,
     shadowOffset: {height: 8, width: 4},
@@ -312,4 +632,8 @@ export default App;
 }
 {
   /* <Button title="Go back" onPress={() => navigation.goBack()} /> */
+  // temp 36.1 -- 37.2
+  // pulse 60 -- 100
+  //oxy 75 -- 100
+  //pres 120/80 -- 139/89
 }
